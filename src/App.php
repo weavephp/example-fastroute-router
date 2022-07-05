d53ded563e6ef350963ff954f215379c65e36958
Robert Goldsmith <r.s.goldsmith@far-blue.co.uk>
2022-07-05 18:40:11 +0100

    Update to php8.x and weave 2.x

--- src/App.php
+++ src/App.php
@@ -11,7 +11,10 @@ namespace App;
  */
 class App
 {
-	use \Weave\Weave, \Weave\Config\Laminas\Laminas, \Weave\Error\Whoops\Whoops, \Weave\Container\Aura\Aura;
+	use \Weave\Weave;
+	use \Weave\Config\Laminas\Laminas;
+	use \Weave\Error\Whoops\Whoops;
+	use \Weave\Container\Aura\Aura;
 	// ^^ Use laminas for config, Whoops for error handling and Aura for the DIC.
 
 	/**
@@ -27,12 +30,12 @@ class App
 	 * the Aura\Di\ContainerConfigInterface interface to configure. In our case, we have an
 	 * instance of App\Config we pass in.
 	 *
-	 * @param array  $config      Optional config array as provided from loadConfig().
-	 * @param string $environment Optional indication of the runtime environment.
+	 * @param array   $config      Optional config array as provided from loadConfig().
+	 * @param ?string $environment Optional indication of the runtime environment.
 	 *
 	 * @return array
 	 */
-	protected function provideContainerConfigs(array $config = [], $environment = null)
+	protected function provideContainerConfigs(array $config = [], ?string $environment = null): array
 	{
 		return [
 			new Config($config)
@@ -59,11 +62,11 @@ class App
 	 * also call a Controller after executing some more middlewares, you can use the
 	 * Dispatch middleware (as shown here) to decide when the Controller is executed.
 	 *
-	 * @param string $pipelineName The name of the pipeline to return a definition for.
+	 * @param ?string $pipelineName The name of the pipeline to return a definition for.
 	 *
 	 * @return mixed Whatever the chosen Middleware stack uses for a pipeline of middlewares.
 	 */
-	protected function provideMiddlewarePipeline($pipelineName = null)
+	protected function provideMiddlewarePipeline(?string $pipelineName = null): mixed
 	{
 		switch ($pipelineName) {
 			case 'uppercaseOwner':
@@ -109,7 +112,7 @@ class App
 	 *
 	 * @return null
 	 */
-	protected function provideRouteConfiguration($router)
+	protected function provideRouteConfiguration(mixed $router): void
 	{
 		$router->get(
 			'/[{owner}]',
